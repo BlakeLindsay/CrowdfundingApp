09cabada441a0500d8126aa3d19a082f51b503d0
Alexander Zic <zic.alexander@gmail.com>
2023-11-20 20:13:01 -0700

    color changes

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -11,11 +11,11 @@ const Login = () => {
           <form className="flex flex-col">                    
             <input
                 placeholder="Email"
-                className="bg-teal-50 text-cyan-900 border-0 rounded-md p-2  mb-4 focus:bg-teal-200 focus:outline-none transition ease-in-out duration-150 placeholder-cyan-900"
+                className="bg-teal-50 text-cyan-900 font-bold border-0 rounded-md p-2  mb-4 focus:bg-teal-100 focus:outline-none transition ease-in-out duration-150 placeholder-cyan-900"
                 type="text" />
               <input
                 placeholder="Password"
-                className="bg-teal-50 text-cyan-900 border-0 rounded-md p-2  mb-4 focus:bg-teal-200 focus:outline-none transition ease-in-out duration-150 placeholder-cyan-900"
+                className="bg-teal-50 text-cyan-900 font-bold border-0 rounded-md p-2  mb-4 focus:bg-teal-100 focus:outline-none transition ease-in-out duration-150 placeholder-cyan-900"
                 type="password" />
                 <button className='bg-teal-500 text-white font-medium py-2 px-4 rounded-md hover:bg-teal-600 transition ease-in duration-200' type="submit">Submit</button>
                 <p className='text-white mt-4 text-center'>Don't have an account?
