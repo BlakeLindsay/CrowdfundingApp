64aae34710d2b6782551058ee720ed63a808a86f
Kevin Soto <Kso040391@gmail.com>
2023-12-03 13:04:03 -0700

    Revert "Bigfixes ii"

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,66 +1,25 @@
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import React from 'react'
 
-  function Login(props) {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const navigate = useNavigate();
-  
-    async function loginUser(e) {
-      e.preventDefault();
-  
-      try {
-				
-        const response = await fetch("http://localhost:4000/user/login", {
-          headers: new Headers({
-            "Content-Type": "application/json",
-          }),
-          method: "POST",
-          body: JSON.stringify({
-            email,
-            password,
-          }),
-        });
-  
-        const results = await response.json();
-        console.log(response.status);
-        if (response.status === 200) {
-          console.log("Login successful");
-          console.log("Token:", results.token);
-					console.log("userID:", results.user._id);
-          props.setToken(results.token, results.user._id);
-          navigate("/"); // Navigate to homepage
-        } else {
-          console.log("Login failed");
-        }
-      } catch (error) {
-        console.log(error);
-      }
-    }
-
-
-
+const Login = () => {
   return (
     <div className="p-5 sm:p-0">
       <div className="flex flex-col items-center justify-center h-screen">
-        <div className="w-full max-w-md bg-cyan-900 rounded-xl shadow-md py-8 px-8 mb-40 md:mt-40">
+        <div className="w-full max-w-md bg-cyan-900 rounded-xl shadow-md py-8 px-8">
           <h2 className="text-[28px] font-bold text-white mb-6 text-center">
             Login
           </h2>
-          <form className="flex flex-col" onSubmit={loginUser}>                    
+          <form className="flex flex-col">                    
             <input
                 placeholder="Email"
-                className="bg-teal-50 text-cyan-900 font-bold border-0 rounded-md p-2  mb-4 focus:bg-teal-100 focus:outline-none transition ease-in-out duration-150 placeholder-cyan-900"
-                type="email" 
-                onChange={(e) => setEmail(e.target.value)}/>
+                className="bg-teal-50 text-cyan-900 border-0 rounded-md p-2  mb-4 focus:bg-teal-200 focus:outline-none transition ease-in-out duration-150 placeholder-cyan-900"
+                type="text" />
               <input
                 placeholder="Password"
-                className="bg-teal-50 text-cyan-900 font-bold border-0 rounded-md p-2  mb-4 focus:bg-teal-100 focus:outline-none transition ease-in-out duration-150 placeholder-cyan-900"
-                type="password" 
-                onChange={(e) => setPassword(e.target.value)}/>
+                className="bg-teal-50 text-cyan-900 border-0 rounded-md p-2  mb-4 focus:bg-teal-200 focus:outline-none transition ease-in-out duration-150 placeholder-cyan-900"
+                type="password" />
                 <button className='bg-teal-500 text-white font-medium py-2 px-4 rounded-md hover:bg-teal-600 transition ease-in duration-200' type="submit">Submit</button>
                 <p className='text-white mt-4 text-center'>Don't have an account?
-                <a className='text-teal-200 hover:underline mt-4 px-1' href='/signup'>Sign Up</a></p>
+                <a className='text-teal-200 hover:underline mt-4 px-1' href='#'>Sign Up</a></p>
                 
           </form>
         </div>
