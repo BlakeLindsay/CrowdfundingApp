c44ee8966323b8635e02c1e16ff5b1be8a97c770
BlakeLindsay <104230616+BlakeLindsay@users.noreply.github.com>
2023-12-09 15:28:53 -0700

    init mock donate

--- src/components/donate/Donate.jsx
+++ src/components/donate/Donate.jsx
@@ -1,77 +1,27 @@
-import {
-	PayPalScriptProvider,
-	PayPalButtons,
-	usePayPalScriptReducer
-} from "@paypal/react-paypal-js";
+import React, { useState, useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
 
-// This value is from the props in the UI
-const style = {"layout":"vertical"};
+function Donate({userID}) {
+	const [amount, setAmount] = useState(0);
+	const [message, setMessage] = useState("");
 
-function Donate() {
-	function createOrder() {
-			// replace this url with your server
-			return fetch("https://react-paypal-js-storybook.fly.dev/api/paypal/create-order", {
-					method: "POST",
-					headers: {
-							"Content-Type": "application/json",
-					},
-					// use the "body" param to optionally pass additional order information
-					// like product ids and quantities
-					body: JSON.stringify({
-							cart: [
-									{
-											sku: "etanod01",
-											quantity: 2,
-									},
-							],
-					}),
-			})
-					.then((response) => response.json())
-					.then((order) => {
-							// Your code here after create the order
-							return order.id;
-					});
-	}
-	function onApprove(data) {
-			// replace this url with your server
-			return fetch("https://react-paypal-js-storybook.fly.dev/api/paypal/capture-order", {
-					method: "POST",
-					headers: {
-							"Content-Type": "application/json",
-					},
-					body: JSON.stringify({
-							orderID: data.orderID,
-					}),
-			})
-					.then((response) => response.json())
-					.then((orderData) => {
-							// Your code here after capture the order
-					});
-	}
-
-	// Custom component to wrap the PayPalButtons and show loading spinner
-	const ButtonWrapper = ({ showSpinner }) => {
-			const [{ isPending }] = usePayPalScriptReducer();
-
-			return (
-					<>
-							{ (showSpinner && isPending) && <div className="spinner" /> }
-							<PayPalButtons
-									fundingSource="paypal"
-									style={{"layout":"vertical","label":"donate"}}
-									disabled={false}
-									forceReRender={[style]}
-									createOrder={createOrder}
-									onApprove={onApprove}
-							/>
-					</>
-			);
+	async function handleSubmit(e) {
+		e.preventDefault();
+		try {
+			
+		} catch (error) {
+			console.log(error);
+		}
 	}
 
 	return (
-		<PayPalScriptProvider options={{ clientId: "AXy-0JzMIvts3Z2ido6QQjaA7uEoTzHiz25pnvRjhAzlrKysP6h0VKj5SlYlj3DThWiPwAgPn7dMWWKG", components: "buttons", currency: "USD" }}>
-      <ButtonWrapper showSpinner={false} />
-    </PayPalScriptProvider>
+		<div>
+			<form onSubmit={handleSubmit}>
+				<input type="number" placeholder="amount" onChange={(e) => setAmount(e.target.value)} />
+				<input type="text" placeholder="message" onChange={(e) => setMessage(e.target.value)} />
+				<button type="submit">Submit</button>
+			</form>
+		</div>
 	);
 }
 
