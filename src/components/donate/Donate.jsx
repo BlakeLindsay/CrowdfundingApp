33915f20f89787087077e9a00a7bd3d7cbc13e9c
BlakeLindsay <104230616+BlakeLindsay@users.noreply.github.com>
2023-12-09 20:18:18 -0700

    should donate properly

--- src/components/donate/Donate.jsx
+++ src/components/donate/Donate.jsx
@@ -4,11 +4,28 @@ import { useLocation, useNavigate } from "react-router-dom";
 function Donate({userID}) {
 	const [amount, setAmount] = useState(0);
 	const [message, setMessage] = useState("");
+	const [donationName, setDonationName] = useState("");
+	const location = useLocation();
+  const { campaignId } = location.state;
 
 	async function handleSubmit(e) {
 		e.preventDefault();
 		try {
-			
+			const response = await fetch("http://localhost:4000/donation", {
+				headers: new Headers({
+					"Content-Type": "application/json",
+				}),
+				method: "POST",
+				body: JSON.stringify({
+					amount,
+					message,
+					donationName,
+					userID,
+					campaignId
+				})
+			});
+			const results = await response.json();
+			console.log(results);
 		} catch (error) {
 			console.log(error);
 		}
@@ -17,6 +34,7 @@ function Donate({userID}) {
 	return (
 		<div>
 			<form onSubmit={handleSubmit}>
+				<input type="text" placeholder="donation name" onChange={(e) => setDonationName(e.target.value)} />
 				<input type="number" placeholder="amount" onChange={(e) => setAmount(e.target.value)} />
 				<input type="text" placeholder="message" onChange={(e) => setMessage(e.target.value)} />
 				<button type="submit">Submit</button>
